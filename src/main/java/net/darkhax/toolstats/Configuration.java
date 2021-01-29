7dcdb1cf22886bd03a1c3e1b6143ad77e4f75a09
Tyler Hancock <darklime@live.ca>
2021-01-29 03:36:10 -0700

    Added config option to always display the enchantability state. #6

--- src/main/java/net/darkhax/toolstats/Configuration.java
+++ src/main/java/net/darkhax/toolstats/Configuration.java
@@ -8,6 +8,7 @@ public class Configuration {
     private final ForgeConfigSpec spec;
     
     private final BooleanValue showEnchantability;
+    private final BooleanValue alwaysEnchantability;
     private final BooleanValue showHarvestLevel;
     private final BooleanValue showEfficiency;
     private final BooleanValue showRepairCost;
@@ -23,6 +24,9 @@ public class Configuration {
         
         builder.comment("Should enchantability be shown?");
         this.showEnchantability = builder.define("show-enchantability", true);
+        
+        builder.comment("Should enchantability always be shown?");
+        this.alwaysEnchantability = builder.define("always-show-enchantability", false);
         
         builder.comment("Should harvest level be shown?");
         this.showHarvestLevel = builder.define("show-harvest-level", true);
@@ -45,28 +49,33 @@ public class Configuration {
         return this.spec;
     }
     
-    public boolean shouldShowEnchantability() {
-    	
-    	return this.showEnchantability.get();
+    public boolean shouldShowEnchantability () {
+        
+        return this.showEnchantability.get();
     }
     
-    public boolean shouldShowHarvestLevel() {
-    	
-    	return this.showHarvestLevel.get();
+    public boolean shouldShowEnchantabilityAlways () {
+        
+        return this.alwaysEnchantability.get();
     }
     
-    public boolean shouldShowEfficiency() {
-    	
-    	return this.showEfficiency.get();
+    public boolean shouldShowHarvestLevel () {
+        
+        return this.showHarvestLevel.get();
     }
     
-    public boolean shouldShowRepairCost() {
-    	
-    	return this.showRepairCost.get();
+    public boolean shouldShowEfficiency () {
+        
+        return this.showEfficiency.get();
     }
     
-    public boolean shouldShowDurability() {
-    	
-    	return this.showDurability.get();
+    public boolean shouldShowRepairCost () {
+        
+        return this.showRepairCost.get();
+    }
+    
+    public boolean shouldShowDurability () {
+        
+        return this.showDurability.get();
     }
 }
--- src/main/java/net/darkhax/toolstats/ToolStats.java
+++ src/main/java/net/darkhax/toolstats/ToolStats.java
@@ -4,6 +4,8 @@ import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.commons.lang3.tuple.Pair;
+
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screen.EnchantmentScreen;
 import net.minecraft.client.gui.screen.inventory.AnvilScreen;
@@ -17,10 +19,12 @@ import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.entity.player.ItemTooltipEvent;
 import net.minecraftforge.eventbus.api.EventPriority;
+import net.minecraftforge.fml.ExtensionPoint;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig.Type;
 import net.minecraftforge.fml.loading.FMLEnvironment;
+import net.minecraftforge.fml.network.FMLNetworkConstants;
 
 @Mod("toolstats")
 public class ToolStats {
@@ -30,85 +34,87 @@ public class ToolStats {
     
     public ToolStats() {
         
-    	if (FMLEnvironment.dist == Dist.CLIENT) {
-    		
-            ModLoadingContext.get().registerConfig(Type.CLIENT, this.config.getSpec());        
-            MinecraftForge.EVENT_BUS.addListener(EventPriority.HIGH, this::onItemTooltip);
-    	}
-    }
-    
-    private void onItemTooltip(ItemTooltipEvent event) {
+        ModLoadingContext.get().registerExtensionPoint(ExtensionPoint.DISPLAYTEST, () -> Pair.of( () -> FMLNetworkConstants.IGNORESERVERONLY, (a, b) -> true));
         
-    	final List<ITextComponent> additions = new ArrayList<>();
-    	
-    	final ItemStack stack = event.getItemStack();
-    	
-    	if (stack.getItem() instanceof TieredItem) {
-    		
-    		final TieredItem item = ((TieredItem) stack.getItem());
-    		final IItemTier tier = item.getTier();
-    		
-    		if (config.shouldShowHarvestLevel()) {
-    			
-    			additions.add(new TranslationTextComponent("tooltip.toolstats.harvestlevel", tier.getHarvestLevel()).mergeStyle(TextFormatting.DARK_GREEN));
-    		}
-    		
-    		if (config.shouldShowEfficiency()) {
-    			
-    			additions.add(new TranslationTextComponent("tooltip.toolstats.efficiency", format.format(tier.getEfficiency())).mergeStyle(TextFormatting.DARK_GREEN));
-    		}
-    	}
-    	
-    	if (config.shouldShowEnchantability() && Minecraft.getInstance().currentScreen instanceof EnchantmentScreen) {
-    		
-    		final int enchantability = stack.getItemEnchantability();
-    		
-    		if (enchantability > 0) {
-    			
-    			additions.add(new TranslationTextComponent("tooltip.toolstats.enchantability", enchantability).mergeStyle(TextFormatting.DARK_GREEN));
-    		}
-    	}
-    	
-    	if (config.shouldShowRepairCost() && Minecraft.getInstance().currentScreen instanceof AnvilScreen) {
-    		
-    		final int repairCost = stack.getRepairCost();
-    		
-    		if (repairCost > 0) {
-    			
-    			additions.add(new TranslationTextComponent("tooltip.toolstats.repaircost", repairCost).mergeStyle(TextFormatting.DARK_GREEN));
-    		}
-    	}
-    	
-    	if (config.shouldShowDurability() && stack.isDamageable()) {
-    		
-    		additions.add(new TranslationTextComponent("item.durability", stack.getMaxDamage() - stack.getDamage(), stack.getMaxDamage()));
-    	}
-    	
-    	event.getToolTip().addAll(getInsertOffset(event.getFlags().isAdvanced(), event.getToolTip().size(), stack), additions);
+        if (FMLEnvironment.dist == Dist.CLIENT) {
+            
+            ModLoadingContext.get().registerConfig(Type.CLIENT, this.config.getSpec());
+            MinecraftForge.EVENT_BUS.addListener(EventPriority.HIGH, this::onItemTooltip);
+        }
     }
     
-    private static int getInsertOffset(boolean advanced, int tooltipSize, ItemStack stack) {
-    	
-    	int offset = 0;
-    	
-    	if (advanced) {
-    		
-    		// item id
-    		offset++;
-    		
-    		// tag count
-    		if (stack.hasTag()) {
-    			
-    			offset++;
-    		}
-    		
-    		// durability
-    		if (stack.isDamaged()) {
-    			
-    			offset++;
-    		}
-    	}
-    	
-    	return Math.max(0, tooltipSize - offset);
+    private void onItemTooltip (ItemTooltipEvent event) {
+        
+        final List<ITextComponent> additions = new ArrayList<>();
+        
+        final ItemStack stack = event.getItemStack();
+        
+        if (stack.getItem() instanceof TieredItem) {
+            
+            final TieredItem item = (TieredItem) stack.getItem();
+            final IItemTier tier = item.getTier();
+            
+            if (this.config.shouldShowHarvestLevel()) {
+                
+                additions.add(new TranslationTextComponent("tooltip.toolstats.harvestlevel", tier.getHarvestLevel()).mergeStyle(TextFormatting.DARK_GREEN));
+            }
+            
+            if (this.config.shouldShowEfficiency()) {
+                
+                additions.add(new TranslationTextComponent("tooltip.toolstats.efficiency", this.format.format(tier.getEfficiency())).mergeStyle(TextFormatting.DARK_GREEN));
+            }
+        }
+        
+        if (this.config.shouldShowEnchantability() && (this.config.shouldShowEnchantabilityAlways() || Minecraft.getInstance().currentScreen instanceof EnchantmentScreen)) {
+            
+            final int enchantability = stack.getItemEnchantability();
+            
+            if (enchantability > 0) {
+                
+                additions.add(new TranslationTextComponent("tooltip.toolstats.enchantability", enchantability).mergeStyle(TextFormatting.DARK_GREEN));
+            }
+        }
+        
+        if (this.config.shouldShowRepairCost() && Minecraft.getInstance().currentScreen instanceof AnvilScreen) {
+            
+            final int repairCost = stack.getRepairCost();
+            
+            if (repairCost > 0) {
+                
+                additions.add(new TranslationTextComponent("tooltip.toolstats.repaircost", repairCost).mergeStyle(TextFormatting.DARK_GREEN));
+            }
+        }
+        
+        if (this.config.shouldShowDurability() && stack.isDamageable()) {
+            
+            additions.add(new TranslationTextComponent("item.durability", stack.getMaxDamage() - stack.getDamage(), stack.getMaxDamage()));
+        }
+        
+        event.getToolTip().addAll(getInsertOffset(event.getFlags().isAdvanced(), event.getToolTip().size(), stack), additions);
+    }
+    
+    private static int getInsertOffset (boolean advanced, int tooltipSize, ItemStack stack) {
+        
+        int offset = 0;
+        
+        if (advanced) {
+            
+            // item id
+            offset++;
+            
+            // tag count
+            if (stack.hasTag()) {
+                
+                offset++;
+            }
+            
+            // durability
+            if (stack.isDamaged()) {
+                
+                offset++;
+            }
+        }
+        
+        return Math.max(0, tooltipSize - offset);
     }
 }
