adfc2f0f34ba06fde16b753915caa34d13bc98a8
Tyler Hancock <darklime@live.ca>
2022-02-28 20:47:45 -0700

    Add support for disabling tooltip info using item tags.
    I do not sync these, and I won't sync them. It's up to Mojang/Forge/Fabric, if they don't sync it that sucks. No warranty included, etc.

--- Common/src/main/java/net/darkhax/toolstats/ToolStatsCommon.java
+++ Common/src/main/java/net/darkhax/toolstats/ToolStatsCommon.java
@@ -2,6 +2,7 @@ package net.darkhax.toolstats;
 
 import it.unimi.dsi.fastutil.ints.Int2ObjectMap;
 import it.unimi.dsi.fastutil.ints.Int2ObjectOpenHashMap;
+import net.darkhax.bookshelf.api.Services;
 import net.darkhax.toolstats.config.ConfigSchema;
 import net.minecraft.ChatFormatting;
 import net.minecraft.client.Minecraft;
@@ -9,6 +10,9 @@ import net.minecraft.client.gui.screens.inventory.AnvilScreen;
 import net.minecraft.client.gui.screens.inventory.EnchantmentScreen;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.TranslatableComponent;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.Tag;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Tier;
 import net.minecraft.world.item.TieredItem;
@@ -22,6 +26,13 @@ import java.util.Map;
 import java.util.function.Function;
 
 public class ToolStatsCommon {
+
+    private static Tag<Item> TAG_IGNORE = itemTag("ignored");
+    private static Tag<Item> TAG_IGNORE_HARVEST_LEVEL = itemTag("ignore_harvest_level");
+    private static Tag<Item> TAG_IGNORE_DIG_SPEED = itemTag("ignore_dig_speed");
+    private static Tag<Item> TAG_IGNORE_ENCHANTABILITY = itemTag("ignore_enchantability");
+    private static Tag<Item> TAG_IGNORE_REPAIR_COST = itemTag("ignore_repair_cost");
+    private static Tag<Item> TAG_IGNORE_DURABILITY = itemTag("ignore_durability");
 
     private final ConfigSchema config;
     private final Function<ItemStack, Integer> enchantabilityResolver;
@@ -41,50 +52,58 @@ public class ToolStatsCommon {
 
     public void displayTooltipInfo(ItemStack stack, TooltipFlag context, List<Component> tooltip) {
 
-        final List<Component> additions = new ArrayList<>();
+        final Item item = stack.getItem();
 
-        if (stack.getItem() instanceof TieredItem tieredItem) {
+        if (!TAG_IGNORE.contains(item)) {
 
-            if (this.config.showHarvestLevel) {
+            final List<Component> additions = new ArrayList<>();
 
-                additions.add(this.harvestLevelCache.computeIfAbsent(harvestLevelResolver.apply(tieredItem.getTier()), lvl -> new TranslatableComponent("tooltip.toolstats.harvestlevel", lvl).withStyle(ChatFormatting.DARK_GREEN)));
+            if (stack.getItem() instanceof TieredItem tieredItem) {
+
+                if (!TAG_IGNORE_HARVEST_LEVEL.contains(item) && this.config.showHarvestLevel) {
+
+                    additions.add(this.harvestLevelCache.computeIfAbsent(harvestLevelResolver.apply(tieredItem.getTier()), lvl -> new TranslatableComponent("tooltip.toolstats.harvestlevel", lvl).withStyle(ChatFormatting.DARK_GREEN)));
+                }
+
+                if (!TAG_IGNORE_DIG_SPEED.contains(item) && this.config.showEfficiency) {
+
+                    additions.add(digSpeedCache.computeIfAbsent(tieredItem.getTier(), tier -> new TranslatableComponent("tooltip.toolstats.efficiency", Constants.DECIMAL_FORMAT.format(tier.getSpeed())).withStyle(ChatFormatting.DARK_GREEN)));
+                }
             }
 
-            if (this.config.showEfficiency) {
+            if (!TAG_IGNORE_ENCHANTABILITY.contains(item) && this.config.showEnchantability && (this.config.alwaysShowEnchantability || Minecraft.getInstance().screen instanceof EnchantmentScreen)) {
 
-                additions.add(digSpeedCache.computeIfAbsent(tieredItem.getTier(), tier -> new TranslatableComponent("tooltip.toolstats.efficiency", Constants.DECIMAL_FORMAT.format(tier.getSpeed())).withStyle(ChatFormatting.DARK_GREEN)));
+                final int enchantability = this.enchantabilityResolver.apply(stack);
+
+                if (enchantability > 0) {
+
+                    additions.add(this.enchantabilityCache.computeIfAbsent(enchantability, enchLvl -> new TranslatableComponent("tooltip.toolstats.enchantability", enchLvl).withStyle(ChatFormatting.DARK_GREEN)));
+                }
+            }
+
+            if (!TAG_IGNORE_REPAIR_COST.contains(item) && this.config.showRepairCost && (this.config.alwaysShowRepairCost || Minecraft.getInstance().screen instanceof AnvilScreen)) {
+
+                final int repairCost = stack.getBaseRepairCost();
+
+                if (repairCost > 0) {
+
+                    additions.add(this.repairCostCache.computeIfAbsent(repairCost, cost -> new TranslatableComponent("tooltip.toolstats.repaircost", cost).withStyle(ChatFormatting.DARK_GREEN)));
+                }
+            }
+
+            if (!context.isAdvanced() && !TAG_IGNORE_DURABILITY.contains(item) && this.config.showDurability && stack.isDamageableItem()) {
+
+                if (this.config.alwaysShowDurability || stack.isDamaged()) {
+
+                    additions.add(new TranslatableComponent("item.durability", stack.getMaxDamage() - stack.getDamageValue(), stack.getMaxDamage()));
+                }
+            }
+
+            if (!additions.isEmpty()) {
+
+                tooltip.addAll(getInsertOffset(context.isAdvanced(), tooltip.size(), stack), additions);
             }
         }
-
-        if (this.config.showEnchantability && (this.config.alwaysShowEnchantability || Minecraft.getInstance().screen instanceof EnchantmentScreen)) {
-
-            final int enchantability = this.enchantabilityResolver.apply(stack);
-
-            if (enchantability > 0) {
-
-                additions.add(this.enchantabilityCache.computeIfAbsent(enchantability, enchLvl -> new TranslatableComponent("tooltip.toolstats.enchantability", enchLvl).withStyle(ChatFormatting.DARK_GREEN)));
-            }
-        }
-
-        if (this.config.showRepairCost && (this.config.alwaysShowRepairCost || Minecraft.getInstance().screen instanceof AnvilScreen)) {
-
-            final int repairCost = stack.getBaseRepairCost();
-
-            if (repairCost > 0) {
-
-                additions.add(this.repairCostCache.computeIfAbsent(repairCost, cost -> new TranslatableComponent("tooltip.toolstats.repaircost", cost).withStyle(ChatFormatting.DARK_GREEN)));
-            }
-        }
-
-        if (!context.isAdvanced() && this.config.showDurability && stack.isDamageableItem()) {
-
-            if (this.config.alwaysShowDurability || stack.isDamaged()) {
-
-                additions.add(new TranslatableComponent("item.durability", stack.getMaxDamage() - stack.getDamageValue(), stack.getMaxDamage()));
-            }
-        }
-
-        tooltip.addAll(getInsertOffset(context.isAdvanced(), tooltip.size(), stack), additions);
     }
 
     private static int getInsertOffset(boolean advanced, int tooltipSize, ItemStack stack) {
@@ -110,5 +129,10 @@ public class ToolStatsCommon {
         }
 
         return Math.max(0, tooltipSize - offset);
+    }
+
+    private static Tag<Item> itemTag(String key) {
+
+        return Services.TAGS.itemTag(new ResourceLocation(Constants.MOD_ID, key));
     }
 }
